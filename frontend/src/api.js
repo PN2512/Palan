9feec74a6f2cb9815110f6299ef695ba02bcf642
Pravin Nagre <pravinnagre312@gmail.com>
2026-09-24 12:36:10 +0530

    updated api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // This pulls the URL from Vercel
+  baseURL: import.meta.env.VITE_API_URL || 'https://palan-mp3q.onrender.com',
+  withCredentials: true,
 });
 
 export default API;
